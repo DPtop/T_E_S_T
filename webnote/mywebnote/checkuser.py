83971e5a70d216df31868201ddd66a750bc54c12
DimOn <dim@on>
2024-01-09 22:04:51 +0300

    small changes

--- webnote/mywebnote/checkuser.py
+++ webnote/mywebnote/checkuser.py
@@ -5,13 +5,13 @@ from urllib.parse import unquote
 
 nick_name = str('')
 pass_word = str('')
-is_registered = str('')
 
 
 def CheckUser(request):
-    global is_registered, nick_name, pass_word
-    is_registered = request     # unquote(request, 'utf-8')
-    print('request :: ', is_registered)
+    print('request :: ', request)
+    global nick_name, pass_word
+    is_registered = str(request)
+    is_registered = unquote(is_registered, 'utf-8')
     # вычленяем ник и пароль
     i, nname_begin, nname_end, pword_begin, pword_end = 0, 0, 0, 0, 0
     while i < len(is_registered):
@@ -20,25 +20,25 @@ def CheckUser(request):
             i += 1
         elif is_registered[i:i+15] == '.end1?Password=':
             nname_end = i
-            nick_name = unquote(is_registered[nname_begin:nname_end], 'utf-8')
-            # nick_name = is_registered[nname_begin:nname_end]
+            nick_name = is_registered[nname_begin:nname_end]
             pword_begin = i + 15
             i += 1
         elif is_registered[i:i+5] == '.end2':
             pword_end = i
-            pass_word = unquote(is_registered[pword_begin:pword_end], 'utf-8')
-            # pass_word = is_registered[pword_begin:pword_end]
+            pass_word = is_registered[pword_begin:pword_end]
         i += 1
+    print('::', nick_name, pass_word)
     # проверка наличия в таблице sql
     with sqlite3.connect("db.sqlite3") as db:
         cur = db.cursor()
         result = cur.execute("SELECT * FROM mywebnote_users;")
         ok = False
         for row in result.fetchall():
-            # print(':2:', 'id:', row[0], ', n:', row[1], ', p:', row[2])
+            # print(':sql:', 'id:', row[0], ', n:', row[1], ', p:', row[2])
             if row[1] == nick_name and row[2] == pass_word:
                 print('the user is ok')
                 ok = True
         if not ok:
             print('NO')
-    return HttpResponse()
+    nick_name = pass_word = ''
+    # return HttpResponse()
