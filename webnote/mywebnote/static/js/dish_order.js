3c959e38ea8f136e22d35e19e2270a314781fe27
DimOn <dim@on>
2024-02-14 21:46:28 +0300

    отправка заказа в процессе

--- webnote/mywebnote/static/js/dish_order.js
+++ webnote/mywebnote/static/js/dish_order.js
@@ -3,6 +3,10 @@ let sumDishOrder = sessionStorage.getItem('sumDish');
 function sumOrder(){
     let buttonSumOrder = document.getElementById('buttonSumOrder');
     sumDishOrder = sumDishOrder.split(', ');
+
+    // Создаем элемент <div> с id 'myDIV'
+    var divElement = document.getElementById('myDIV');
+
     for (let i = 0; i < sumDishOrder.length; i++){
         let ordImg = sumDishOrder[i].split('..');
         if (ordImg != ''){
@@ -10,26 +14,49 @@ function sumOrder(){
             ordImg0 = ordImg0.replace(/\s/g, "");
             ordImg1 = ordImg[1];
 
-            // Создаем элементы
-            const divElement = document.createElement('div');
-            const imgElement = document.createElement('img');
-            const pElement = document.createElement('p');
+//            // Создаем элементы
+//            const divElement = document.createElement('div');
+//            const imgElement = document.createElement('img');
+//            const pElement = document.createElement('p');
+//
+//            // Устанавливаем текст для элемента <p>
+//            pElement.textContent = 'в количестве порций: ' + ordImg1;
+//
+//            // Устанавливаем атрибуты для элемента <img>
+//            imgElement.setAttribute('src', '/static/image/'+ordImg0.toLowerCase()+'.jpeg');
+////            imgElement.setAttribute('alt', 'Текстовое описание изображения');
+//
+//            // Добавляем элементы <img> и <p> внутрь элемента <div>
+//            divElement.appendChild(imgElement);
+//            divElement.appendChild(pElement);
+//
+//            // Добавляем текстовое содержимое для элемента <div>
+////            divElement.textContent += ' Дополнительный текст';
+//
+//            // Добавляем элемент <div> в DOM
+//            document.body.appendChild(divElement);
 
-            // Устанавливаем текст для элемента <p>
-            pElement.textContent = 'в количестве порций: ' + ordImg1;
 
-            // Устанавливаем атрибуты для элемента <img>
-            imgElement.setAttribute('src', '/static/image/'+ordImg0.toLowerCase()+'.jpeg');
-//            imgElement.setAttribute('alt', 'Текстовое описание изображения');
 
-            // Добавляем элементы <img> и <p> внутрь элемента <div>
+            // Создаем элемент <p> с текстом "1234"
+            var paragraphElement = document.createElement('p');
+            paragraphElement.textContent = 'в количестве порций: ' + ordImg1;
+
+            // Создаем элемент <img> и задаем ему атрибуты
+            var imgElement = document.createElement('img');
+            imgElement.src = '/static/image/'+ordImg0.toLowerCase()+'.jpeg';
+//            imgElement.alt = 'Your Image';
+
+            // Создаем второй элемент <p>
+//            var secondParagraphElement = document.createElement('p');
+//            secondParagraphElement.textContent = "Another paragraph inside the <div>";
+
+            // Добавляем созданные элементы внутрь элемента <div>
             divElement.appendChild(imgElement);
-            divElement.appendChild(pElement);
+            divElement.appendChild(paragraphElement);
+//            divElement.appendChild(secondParagraphElement);
 
-            // Добавляем текстовое содержимое для элемента <div>
-//            divElement.textContent += ' Дополнительный текст';
-
-            // Добавляем элемент <div> в DOM
+            // Добавляем элемент <div> на страницу
             document.body.appendChild(divElement);
         }
     };
