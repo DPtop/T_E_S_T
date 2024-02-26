e3a14a2a6ed5325a0d4fb458fc2015a6cf69c058
DimOn <dim@on>
2024-02-26 15:34:04 +0300

    мелкие правки

--- webnote/mywebnote/static/js/dish_order.js
+++ webnote/mywebnote/static/js/dish_order.js
@@ -1,39 +1,56 @@
 let sumDishOrder = sessionStorage.getItem('sumDish');
 
+console.log('sumDishOrder', sumDishOrder);
+
 function sumOrder(){
     // Отображаем кнопку buttonClearOrder
     let btnClear = document.getElementById('buttonClearOrder');
     btnClear.style.visibility = 'visible';
 
-    let buttonSumOrder = document.getElementById('buttonSumOrder');
-    sumDishOrder = sumDishOrder.split(', ');
-
     // Создаем элемент <div> с id 'orderDIV'
     let divElement = document.getElementById('orderDIV');
 
-    for (let i = 0; i < sumDishOrder.length; i++){
-        let ordImg = sumDishOrder[i].split('..');
-        if (ordImg != ''){
-            ordImg0 = ordImg[0].toLowerCase();
-            ordImg0 = ordImg0.replace(/\s/g, "");
-            ordImg1 = ordImg[1];
 
-            // Создаем элемент <p> с текстом "1234"
-            let paragraphElement = document.createElement('p');
-            paragraphElement.textContent = 'в количестве порций: ' + ordImg1;
+    if (sumDishOrder == null) {
+        // Создаем элемент <img> и задаем ему атрибуты
+        let imgElement = document.createElement('img');
+        imgElement.src = '/static/image/null.jpeg';
 
-            // Создаем элемент <img> и задаем ему атрибуты
-            let imgElement = document.createElement('img');
-            imgElement.src = '/static/image/'+ordImg0.toLowerCase()+'.jpeg';
+        // Добавляем созданные элементы внутрь элемента <div>
+        divElement.appendChild(imgElement);
+        divElement.appendChild(paragraphElement);
 
-            // Добавляем созданные элементы внутрь элемента <div>
-            divElement.appendChild(imgElement);
-            divElement.appendChild(paragraphElement);
+        // Добавляем элемент <div> на страницу
+        document.body.appendChild(divElement);
+    }
+    else {
+        let buttonSumOrder = document.getElementById('buttonSumOrder');
+        sumDishOrder = sumDishOrder.split(', ');
 
-            // Добавляем элемент <div> на страницу
-            document.body.appendChild(divElement);
-        }
-    };
+        for (let i = 0; i < sumDishOrder.length; i++){
+            let ordImg = sumDishOrder[i].split('..');
+            if (ordImg != ''){
+                ordImg0 = ordImg[0].toLowerCase();
+                ordImg0 = ordImg0.replace(/\s/g, "");
+                ordImg1 = ordImg[1];
+
+                // Создаем элемент <p> с текстом "1234"
+                let paragraphElement = document.createElement('p');
+                paragraphElement.textContent = 'в количестве порций: ' + ordImg1;
+
+                // Создаем элемент <img> и задаем ему атрибуты
+                let imgElement = document.createElement('img');
+                imgElement.src = '/static/image/'+ordImg0.toLowerCase()+'.jpeg';
+
+                // Добавляем созданные элементы внутрь элемента <div>
+                divElement.appendChild(imgElement);
+                divElement.appendChild(paragraphElement);
+
+                // Добавляем элемент <div> на страницу
+                document.body.appendChild(divElement);
+            }
+        };
+    }
 }
 
 function dishClear(){
